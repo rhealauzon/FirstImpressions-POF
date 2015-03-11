6857c1059db03dc42a05c4293e9d57c8dc1155b4
Calvin Rempel <rempel.calvin@gmail.com>
2015-03-11 10:25:29 -0700

    -Geofencing complete

--- mobile/src/main/java/ca/calvinrempel/firstimpressions_pof/GeofenceListenerCallbacks.java
+++ mobile/src/main/java/ca/calvinrempel/firstimpressions_pof/GeofenceListenerCallbacks.java
@@ -1,13 +1,14 @@
 package ca.calvinrempel.firstimpressions_pof;
 
 /**
+ * GeofenceListnerCallbacks can be implemented to provide functionality on geofencing events.
+ *
  * Created by Calvin on 2015-03-10.
  */
 public interface GeofenceListenerCallbacks
 {
-    public abstract void onEnter(String fenceId);
-    public abstract void onExit(String fenceId);
-    public abstract void onConnected();
-    public abstract void onDisconnected();
-    public abstract void onError();
+    public abstract void onNowHere(FencedMeeting meeting);
+    public abstract void onNoLongerHere(FencedMeeting meeting);
+    public abstract void onNowNear(FencedMeeting meeting);
+    public abstract void onNoLongerNear(FencedMeeting meeting);
 }
